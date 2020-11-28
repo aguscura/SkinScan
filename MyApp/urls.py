d3ff7d08d6026e017c46afbc73fa9370f68b551b
unknown <aguscura9@gmail.com>
2020-11-27 21:33:24 -0300

    comit

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+router = routers.DefaultRouter()
 router.register("MyApp", views.operacionViews)
 
 urlpatterns = [
